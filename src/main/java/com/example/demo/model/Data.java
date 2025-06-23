da34332b7fb32e3901e52025f27decbce02a811e
briansomogyi <brian.somogyi@emanuel.ro>
2025-06-23 15:37:17 +0300

    Added User.java and Data.java models

---
+++ src/main/java/com/example/demo/model/Data.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+@lombok.Data
+public class Data {
+
+    public User user;
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    public String id;
+    public String name;
+    public Integer year;
+    public String color;
+    public String pantome_value;
+
+}
